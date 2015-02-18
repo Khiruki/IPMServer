b6284f965f311f4e7360d2c93234ff3c41544f68
R.Jia <isenet@gmail.com>
2015-02-18 03:59:56 -0500

    Adding CORS filter

--- src/main/java/server/ResponseController.java
+++ src/main/java/server/ResponseController.java
@@ -4,19 +4,16 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import com.google.gson.Gson;
 
 @RestController
 public class ResponseController 
 {
-
-    private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
-
+	private final String noResponse = "Sorry! We were unable to match your query with a response.";
+	
     @RequestMapping("/response")
-    public Response response(@RequestParam(value="name", defaultValue="World") String name) 
-	{
-        return new Response(counter.incrementAndGet(),
-                            String.format(template, name));
+    public Response response(@RequestParam(value="data", defaultValue=noResponse) String data) 
+	{	   
+        return new Response(counter.incrementAndGet(), data);		
     }
 }
